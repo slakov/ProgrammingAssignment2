4628f22951cc70dc0d0a5994b235f26a41a886ac
slakov <slakov@gmail.com>
2014-05-22 11:53:36 +0200

    my version of Ass2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Computing inverse matrix is computationaly expencive therefore we create 
+## a "cached" version of this procedure, so previously cmoputed value can be reused
 
-## Write a short comment describing this function
-
+# This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # if the inverse has already been calculated then retrieve from cache
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                return(s)
+        }
+        
+        # otherwise compute inverse
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
+
+
+# Optional benchmark:
+#
+# x <- matrix(rnorm(10^6), nrow=10^3, ncol=10^3)
+# 
+# a <- Sys.time()
+# solve(x)
+# t1 <-Sys.time() - a
+# print(t)
+# 
+# a <- Sys.time()
+# solve(x)
+# t2 <-Sys.time() - a
+# print(t)
+# 
+# s <- makeCacheMatrix(x)
+# a <- Sys.time()
+# cacheSolve(s)
+# t3 <-Sys.time() - a
+# print(t)
+# 
+# a <- Sys.time()
+# cacheSolve(s)
+# t4 <-Sys.time() - a
+# print(t)
+
